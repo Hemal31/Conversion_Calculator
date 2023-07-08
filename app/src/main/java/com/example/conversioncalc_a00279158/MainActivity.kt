ff80932441222875e4b3bd7247cedd753e163021
Hemal31 <133526950+Hemal31@users.noreply.github.com>
2023-07-07 22:26:45 -0400

    heading
    I developed

---
+++ app/src/main/java/com/example/conversioncalc_a00279158/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.conversioncalc_a00279158
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
